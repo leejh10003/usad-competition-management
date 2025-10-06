21aeb2f5bec5fc65744747b582dcab8b543e6fdd
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-10-06 22:10:53 +0900

    Add hyperdrive for cockroachDB

--- infra/main.ts
+++ infra/main.ts
@@ -9,6 +9,11 @@ import { Project as VercelProject } from './.gen/providers/vercel/project';
 import { D1Database } from '@cdktf/provider-cloudflare/lib/d1-database';
 import { LocalProvider } from '@cdktf/provider-local/lib/provider';
 import { File } from '@cdktf/provider-local/lib/file';
+import { HyperdriveConfig } from '@cdktf/provider-cloudflare/lib/hyperdrive-config';
+import { Cluster as CockroachCluster } from './.gen/providers/cockroach/cluster';
+import { CockroachProvider } from './.gen/providers/cockroach/provider';
+import { DataCockroachConnectionString } from './.gen/providers/cockroach/data-cockroach-connection-string';
+import { SqlUser } from './.gen/providers/cockroach/sql-user';
 
 dotenv.config();
 
@@ -17,6 +22,9 @@ dotenv.config();
 const CLOUDFLARE_ACCOUNT_ID = process.env.CLOUDFLARE_ACCOUNT_ID as string;
 const CLOUDFLARE_API_TOKEN = process.env.CLOUDFLARE_API_TOKEN as string;
 const VERCEL_API_TOKEN = process.env.VERCEL_API_TOKEN as string;
+const COCKROACH_API_KEY = process.env.COCKROACH_API_KEY as string;
+const COCKROACH_SQL_USERNAME = process.env.COCKROACH_SQL_USERNAME as string;
+const COCKROACH_SQL_USER_PASSWORD = process.env.COCKROACH_SQL_USER_PASSWORD as string;
 
 class MyUsadPocStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
@@ -37,6 +45,45 @@ class MyUsadPocStack extends TerraformStack {
       readReplication: {
         mode: "disabled"
       }
+    });
+    const cockroachProvider = new CockroachProvider(this, "cockroachdb", {
+      apikey: COCKROACH_API_KEY,
+    });
+    const cockroachCluster = new CockroachCluster(this, "usad-cockroachdb-cluster", {
+      name: "usad-cockroachdb-cluster",
+      provider: cockroachProvider,
+      cloudProvider: "GCP",
+      plan: "BASIC",
+      regions: [{
+        name: "us-central1",
+      }],
+      serverless: {},
+    });
+    const cockroachUser = new SqlUser(this, "cockroach-sql-user", {
+      password: COCKROACH_SQL_USER_PASSWORD,
+      clusterId: cockroachCluster.id,
+      name: COCKROACH_SQL_USERNAME,
+    });
+    const connectionString = new DataCockroachConnectionString(this, "cockroach-connection-string", {
+      id: cockroachCluster.id,
+      sqlUser: COCKROACH_SQL_USERNAME,
+      password: COCKROACH_SQL_USER_PASSWORD,
+    });
+    const hyperdrive = new HyperdriveConfig(this, "usad-hyperdrive", {
+      name: "usad-hyperdrive",
+      accountId: CLOUDFLARE_ACCOUNT_ID,
+      dependsOn: [connectionString, cockroachCluster, cockroachUser],
+      origin: {
+        database: "postgres",
+        host: connectionString.connectionParams.host,
+        password: COCKROACH_SQL_USER_PASSWORD,
+        port: Fn.parseint(connectionString.connectionParams.port, 10),
+        scheme: "postgres",
+        user: COCKROACH_SQL_USERNAME,
+      },
+      caching: {
+        disabled: false,
+      },
     });
     new Worker(this, "usad-worker", {
       accountId: CLOUDFLARE_ACCOUNT_ID,
@@ -62,7 +109,8 @@ class MyUsadPocStack extends TerraformStack {
       content: Fn.templatefile(
         path.join(__dirname, '..', 'apps', 'worker', 'wrangler.toml.tftpl'),
         {
-          database_id: d1Db.id
+          database_id: d1Db.id,
+          hyperdrive_id: hyperdrive.id,
         }
       ),
       filename: path.join(__dirname, '..', 'apps', 'worker', 'wrangler.toml'),
