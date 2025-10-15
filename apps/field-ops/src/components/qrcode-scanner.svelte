da0f6ec7e8b6a515171264e4697bb074056aa355
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-10-15 11:48:58 +0900

    Remove deprecated codebase

---
+++ apps/field-ops/src/components/qrcode-scanner.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import {
+    Html5QrcodeScanner,
+    type Html5QrcodeResult,
+    Html5QrcodeScanType,
+    Html5QrcodeSupportedFormats,
+    Html5QrcodeScannerState,
+  } from 'html5-qrcode';
+
+  interface QrCodeScannerProps {
+    detect: (e: Html5QrcodeResult) => void;
+    error: (message: string) => void;
+    class: string;
+    width: number;
+    height: number;
+    paused: boolean;
+  }
+  const { detect, error, class: className = '', width, height, paused = false }: QrCodeScannerProps = $props();
+
+  function onScanSuccess(_: string, decodedResult: Html5QrcodeResult): void {
+    detect(decodedResult);
+  }
+
+  // usually better to ignore and keep scanning
+  function onScanFailure(message: string) {
+    error(message);
+  }
+
+  let scanner: Html5QrcodeScanner;
+  onMount(() => {
+    scanner = new Html5QrcodeScanner(
+      'qr-scanner',
+      {
+        fps: 10,
+        qrbox: { width, height },
+        aspectRatio: 1,
+        supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA],
+        formatsToSupport: [Html5QrcodeSupportedFormats.QR_CODE],
+      },
+      false // non-verbose
+    );
+    scanner.render(onScanSuccess, onScanFailure);
+  });
+
+  // pause/resume scanner to avoid unintended scans
+  $effect(() => togglePause(paused));
+  function togglePause(paused: boolean): void {
+    if (paused && scanner?.getState() === Html5QrcodeScannerState.SCANNING) {
+      scanner?.pause();
+    } else if (scanner?.getState() === Html5QrcodeScannerState.PAUSED) {
+      scanner?.resume();
+    }
+  }
+</script>
+
+<div id="qr-scanner" class={className}></div>
--- apps/field-ops/src/routes/event-check-in/+page.svelte
+++ apps/field-ops/src/routes/event-check-in/+page.svelte
@@ -1,1 +1,11 @@
-<h1>Test</h1>
+<script>
+    import QrCodeScanner from "../../components/qrcode-scanner.svelte";
+</script>
+<QrCodeScanner
+  detect={(e) => console.log({ type: 'DETECT', ...e })}
+  error={(e) => console.log({type: 'ERROR', message: e})}
+  paused={false}
+  width={320}
+  height={320}
+  class="w-full max-w-sm"
+/>
