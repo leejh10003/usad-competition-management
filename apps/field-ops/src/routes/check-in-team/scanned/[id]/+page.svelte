87a2de440aa93687881223dab208d2c99152b9ff
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-10-28 01:44:25 +0900

    Add id reference

--- apps/field-ops/src/routes/check-in-team/scanned/[id]/+page.svelte
+++ apps/field-ops/src/routes/check-in-team/scanned/[id]/+page.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+    import { page } from "$app/state";
+    let path = $derived(page.url.pathname);
+    let pathElements = $derived(path.split('/').filter((e) => e.trim().length > 0));
+    let id = $derived(pathElements[pathElements.length - 1]);
+</script>
 <div>
-    success
+    {id}
 </div>
