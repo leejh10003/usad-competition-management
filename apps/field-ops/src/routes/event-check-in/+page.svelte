0e77c585ccf7a1be21d218466563d2f19a806668
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-10-28 01:33:47 +0900

    Format code

--- apps/field-ops/src/routes/event-check-in/+page.svelte
+++ apps/field-ops/src/routes/event-check-in/+page.svelte
@@ -1,11 +1,15 @@
 <script>
-    import QrCodeScanner from "../../components/qrcode-scanner.svelte";
+	import { Block } from 'konsta/svelte';
+	import QrCodeScanner from '../../components/qrcode-scanner.svelte';
 </script>
-<QrCodeScanner
-  detect={(e) => console.log({ type: 'DETECT', ...e })}
-  error={(e) => console.log({type: 'ERROR', message: e})}
-  paused={false}
-  width={320}
-  height={320}
-  class="w-full max-w-sm"
-/>
+
+<Block>
+	<QrCodeScanner
+		detect={(e) => console.log({ type: 'DETECT', ...e })}
+		error={(e) => console.log({ type: 'ERROR', message: e })}
+		paused={false}
+		width={320}
+		height={320}
+		class="w-full max-w-sm"
+	/>
+</Block>
