f9bb5831b2cbe96358d9b80c7f9816edb040022f
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-10-28 03:05:12 +0900

    Proof the concept of binding element

--- apps/field-ops/src/routes/check-in-team/scanned/[id]/+page.svelte
+++ apps/field-ops/src/routes/check-in-team/scanned/[id]/+page.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
     import { page } from "$app/state";
+    import SignaturePad from '../../../../components/signature.svelte';
+    import { Button } from 'konsta/svelte';
     let path = $derived(page.url.pathname);
     let pathElements = $derived(path.split('/').filter((e) => e.trim().length > 0));
     let id = $derived(pathElements[pathElements.length - 1]);
+    var pads: SignaturePad[] = new Array(3);
 </script>
 <div>
-    {id}
+    {#each pads as _, i}
+        <div>
+            <SignaturePad signatureId={id} bind:this={pads[i]}/>
+            <Button onclick={()=> pads[i]?.clear()}>Clear</Button>
+        </div>
+    {/each}
 </div>
