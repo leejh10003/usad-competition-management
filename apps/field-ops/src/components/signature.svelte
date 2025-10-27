e3626438eec5b66c05e732d15da6f7d5c65b98ef
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-10-28 03:04:50 +0900

    Add handwriting component

---
+++ apps/field-ops/src/components/signature.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Button } from 'konsta/svelte';
+    import SignaturePad from 'signature_pad';
+    interface SignatureCanvasProps {
+        signatureId: string
+    }
+    const {signatureId}: SignatureCanvasProps = $props();
+    var signaturePad: SignaturePad;
+    export function getSignature() {
+        return signaturePad?.toDataURL();
+    }
+    export function clear() {
+        signaturePad?.clear();
+    }
+    function initialized(node: HTMLCanvasElement) {
+        signaturePad = new SignaturePad(node);
+    }
+</script>
+<div>
+    <canvas width="300px" height="300px" id={`signature-canvas-${signatureId}`} use:initialized></canvas>
+</div>
