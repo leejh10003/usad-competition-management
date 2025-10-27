41e260c3e077f434170ffe629d074744380ed819
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-10-28 01:33:16 +0900

    Align scanner center

---
+++ apps/field-ops/src/routes/check-in-team/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { ActionsButton, Block, BlockTitle, Button } from 'konsta/svelte';
+	import QrCodeScanner from '../../components/qrcode-scanner.svelte';
+	import { goto } from '$app/navigation';
+</script>
+
+<div class="flex grow flex-col">
+    <Button onclick={() => goto('scanned/test')}>Success</Button>
+	<QrCodeScanner
+		detect={(e) => console.log({ type: 'DETECT', ...e })}
+		error={(e) => console.log({ type: 'ERROR', message: e })}
+		paused={false}
+		width={320}
+		height={320}
+		class="max-w-sm m-auto"
+	/>
+</div>
