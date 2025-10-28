7d5f1bcb83c6756d1a5e55596c3ba613258deb0f
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-10-29 01:51:31 +0900

    Add mount loading logic

--- apps/field-ops/src/routes/check-in-team/scanned/[id]/+page.svelte
+++ apps/field-ops/src/routes/check-in-team/scanned/[id]/+page.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
     import { page } from "$app/state";
+    import { onMount } from "svelte";
     import SignaturePad from '../../../../components/signature.svelte';
     import { Button } from 'konsta/svelte';
+    import _ from 'lodash';
     let path = $derived(page.url.pathname);
     let pathElements = $derived(path.split('/').filter((e) => e.trim().length > 0));
     let id = $derived(pathElements[pathElements.length - 1]);
-    var pads: SignaturePad[] = new Array(3);
+    var pads: SignaturePad[] = $state(new Array(0));
+    onMount(async () =>  {
+        pads = new Array(10);
+    })
 </script>
-<div>
+<div class="flex">
     {#each pads as _, i}
         <div>
             <SignaturePad signatureId={id} bind:this={pads[i]}/>
