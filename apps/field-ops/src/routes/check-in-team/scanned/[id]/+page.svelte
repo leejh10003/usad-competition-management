87e3efc988f62fbd6041699b35d6b818f29eb4e4
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-10-29 01:56:26 +0900

    Add contents

--- apps/field-ops/src/routes/check-in-team/scanned/[id]/+page.svelte
+++ apps/field-ops/src/routes/check-in-team/scanned/[id]/+page.svelte
@@ -1,22 +1,35 @@
 <script lang="ts">
-    import { page } from "$app/state";
-    import { onMount } from "svelte";
-    import SignaturePad from '../../../../components/signature.svelte';
-    import { Button } from 'konsta/svelte';
-    import _ from 'lodash';
-    let path = $derived(page.url.pathname);
-    let pathElements = $derived(path.split('/').filter((e) => e.trim().length > 0));
-    let id = $derived(pathElements[pathElements.length - 1]);
-    var pads: SignaturePad[] = $state(new Array(0));
-    onMount(async () =>  {
-        pads = new Array(10);
-    })
+	import { page } from '$app/state';
+	import { onMount } from 'svelte';
+	import SignaturePad from '../../../../components/signature.svelte';
+	import { Block, BlockTitle, Button } from 'konsta/svelte';
+	import _ from 'lodash';
+	let path = $derived(page.url.pathname);
+	let pathElements = $derived(path.split('/').filter((e) => e.trim().length > 0));
+	let id = $derived(pathElements[pathElements.length - 1]);
+	var pads: SignaturePad[] = $state(new Array(0));
+	onMount(async () => {
+		pads = new Array(10);
+	});
 </script>
-<div class="flex">
-    {#each pads as _, i}
-        <div>
-            <SignaturePad signatureId={id} bind:this={pads[i]}/>
-            <Button onclick={()=> pads[i]?.clear()}>Clear</Button>
-        </div>
-    {/each}
+
+<div class="flex flex-col">
+	<BlockTitle>Code of conduct</BlockTitle>
+	<Block>
+		<p>
+			Donec et nulla auctor massa pharetra adipiscing ut sit amet sem. Suspendisse molestie velit
+			vitae mattis tincidunt. Ut sit amet quam mollis, vulputate turpis vel, sagittis felis.
+		</p>
+	</Block>
+    <BlockTitle>Signatures</BlockTitle>
+	<Block>
+		<div class="flex flex-row">
+			{#each pads as _, i}
+				<div>
+					<SignaturePad signatureId={id} bind:this={pads[i]} />
+					<Button onclick={() => pads[i]?.clear()}>Clear</Button>
+				</div>
+			{/each}
+		</div>
+	</Block>
 </div>
