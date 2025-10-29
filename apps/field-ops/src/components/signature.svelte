09d8b8efa9fad726bae230701a0e06c38b5da5c3
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-10-30 00:22:59 +0900

    Style signature forms

--- apps/field-ops/src/components/signature.svelte
+++ apps/field-ops/src/components/signature.svelte
@@ -1,21 +1,37 @@
 <script lang="ts">
 	import { Button } from 'konsta/svelte';
-    import SignaturePad from 'signature_pad';
-    interface SignatureCanvasProps {
-        signatureId: string
-    }
-    const {signatureId}: SignatureCanvasProps = $props();
-    var signaturePad: SignaturePad;
-    export function getSignature() {
-        return signaturePad?.toDataURL();
-    }
-    export function clear() {
-        signaturePad?.clear();
-    }
-    function initialized(node: HTMLCanvasElement) {
-        signaturePad = new SignaturePad(node);
-    }
+	import SignaturePad from 'signature_pad';
+	interface SignatureCanvasProps {
+		signatureId: string;
+		width?: number;
+		height?: number;
+	}
+	const { signatureId, width, height }: SignatureCanvasProps = $props();
+	var signaturePad: SignaturePad;
+	export function getSignature() {
+		return signaturePad?.toDataURL();
+	}
+	export function clear() {
+		signaturePad?.clear();
+	}
+	function initialized(node: HTMLCanvasElement) {
+		signaturePad = new SignaturePad(node);
+	}
 </script>
+
 <div>
-    <canvas width="300px" height="300px" id={`signature-canvas-${signatureId}`} use:initialized></canvas>
+	<canvas
+        class="signature-canvas"
+		width={`${width ?? 300}px`}
+		height={`${height ?? 300}px`}
+		id={`signature-canvas-${signatureId}`}
+		use:initialized
+	></canvas>
 </div>
+
+<style>
+	.signature-canvas {
+		border: 1px solid #000;
+        border-radius: 10px;
+	}
+</style>
--- apps/field-ops/src/routes/check-in-team/scanned/[id]/+page.svelte
+++ apps/field-ops/src/routes/check-in-team/scanned/[id]/+page.svelte
@@ -13,7 +13,14 @@
 		pads = new Array(10);
 	});
 </script>
-
+<style>
+    .signature-group {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+        gap: 1rem;
+        justify-items: center;
+    }
+</style>
 <div class="flex flex-col">
 	<BlockTitle>Code of conduct</BlockTitle>
 	<Block>
@@ -26,9 +33,10 @@
 	<Block>
 		<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-4">
 			{#each pads as _, i}
-				<div>
-					<SignaturePad signatureId={id} bind:this={pads[i]} />
-					<Button onclick={() => pads[i]?.clear()}>Clear</Button>
+				<div class="signature-group">
+                    <h1 class="text-center font-medium">Signature {i + 1}</h1>
+					<SignaturePad width={300} height={150} signatureId={id} bind:this={pads[i]} />
+					<Button class="w-min" rounded onclick={() => pads[i]?.clear()}>Clear</Button>
 				</div>
 			{/each}
 		</div>
