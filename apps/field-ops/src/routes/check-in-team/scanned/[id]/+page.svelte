323c92e50bb56ef05aa98c3f31bb69f8ba47d42c
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-10-30 00:47:22 +0900

    Add groups for signatures

--- apps/field-ops/src/routes/check-in-team/scanned/[id]/+page.svelte
+++ apps/field-ops/src/routes/check-in-team/scanned/[id]/+page.svelte
@@ -4,42 +4,118 @@
 	import SignaturePad from '../../../../components/signature.svelte';
 	import { Block, BlockTitle, Button } from 'konsta/svelte';
 	import _ from 'lodash';
-    import BottomPadding from '../../../../components/bottom-padding.svelte';
+	import BottomPadding from '../../../../components/bottom-padding.svelte';
 	let path = $derived(page.url.pathname);
 	let pathElements = $derived(path.split('/').filter((e) => e.trim().length > 0));
 	let id = $derived(pathElements[pathElements.length - 1]);
-	var pads: SignaturePad[] = $state(new Array(0));
+	var signatures: {
+		coach?: SignaturePad;
+		honors: (SignaturePad | undefined)[];
+		scholastic: (SignaturePad | undefined)[];
+		varsity: (SignaturePad | undefined)[];
+	} = $state({
+		coach: undefined,
+		honors: [],
+		scholastic: [],
+		varsity: []
+	});
 	onMount(async () => {
-		pads = new Array(10);
+		signatures = {
+			coach: undefined,
+			honors: _.range(3).map(() => undefined),
+			scholastic: _.range(3).map(() => undefined),
+			varsity: _.range(3).map(() => undefined)
+		};
 	});
 </script>
-<style>
-    .signature-group {
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
-        gap: 1rem;
-        justify-items: center;
-    }
-</style>
+
 <div class="flex flex-col">
-	<BlockTitle>Code of conduct</BlockTitle>
+	<BlockTitle large>Code of conduct</BlockTitle>
 	<Block>
 		<p>
 			Donec et nulla auctor massa pharetra adipiscing ut sit amet sem. Suspendisse molestie velit
 			vitae mattis tincidunt. Ut sit amet quam mollis, vulputate turpis vel, sagittis felis.
 		</p>
 	</Block>
-    <BlockTitle>Signatures</BlockTitle>
+	<BlockTitle large>Signatures</BlockTitle>
+	<BlockTitle medium>Coach</BlockTitle>
 	<Block>
-		<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-4">
-			{#each pads as _, i}
+		<div class="signature-group flex flex-col">
+			<h1 class="text-center font-medium">Coach</h1>
+			<SignaturePad width={300} height={150} signatureId={id} bind:this={signatures.coach} />
+			<Button class="w-min" rounded onclick={() => signatures.coach?.clear()}>Clear</Button>
+		</div>
+	</Block>
+	<BlockTitle medium>Honors</BlockTitle>
+	<Block>
+		<div
+			class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3"
+		>
+			{#each signatures.honors as _, i}
 				<div class="signature-group">
-                    <h1 class="text-center font-medium">Signature {i + 1}</h1>
-					<SignaturePad width={300} height={150} signatureId={id} bind:this={pads[i]} />
-					<Button class="w-min" rounded onclick={() => pads[i]?.clear()}>Clear</Button>
+					<h1 class="text-center font-medium">Signature {i + 1}</h1>
+					<SignaturePad
+						width={300}
+						height={150}
+						signatureId={id}
+						bind:this={signatures.honors[i]}
+					/>
+					<Button class="w-min" rounded onclick={() => signatures.honors[i]?.clear()}>Clear</Button>
 				</div>
 			{/each}
 		</div>
 	</Block>
-    <BottomPadding />
+	<BlockTitle medium>Scholastic</BlockTitle>
+	<Block>
+		<div
+			class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3"
+		>
+			{#each signatures.scholastic as _, i}
+				<div class="signature-group">
+					<h1 class="text-center font-medium">Signature {i + 1}</h1>
+					<SignaturePad
+						width={300}
+						height={150}
+						signatureId={id}
+						bind:this={signatures.scholastic[i]}
+					/>
+					<Button class="w-min" rounded onclick={() => signatures.scholastic[i]?.clear()}
+						>Clear</Button
+					>
+				</div>
+			{/each}
+		</div>
+	</Block>
+	<BlockTitle medium>Varsity</BlockTitle>
+	<Block>
+		<div
+			class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3"
+		>
+			{#each signatures.varsity as _, i}
+				<div class="signature-group">
+					<h1 class="text-center font-medium">Signature {i + 1}</h1>
+					<SignaturePad
+						width={300}
+						height={150}
+						signatureId={id}
+						bind:this={signatures.varsity[i]}
+					/>
+					<Button class="w-min" rounded onclick={() => signatures.varsity[i]?.clear()}>Clear</Button
+					>
+				</div>
+			{/each}
+		</div>
+	</Block>
+	<BottomPadding />
 </div>
+
+<style>
+	.signature-group {
+		display: flex;
+		flex-direction: column;
+		width: fit-content;
+		gap: 1rem;
+		justify-items: center;
+		align-items: center;
+	}
+</style>
