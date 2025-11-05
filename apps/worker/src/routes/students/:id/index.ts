ba106f37e9798df627c97dc16d0f10a0995d78df
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-11-05 17:41:13 +0900

    Update one student

--- apps/worker/src/routes/students/:id/index.ts
+++ apps/worker/src/routes/students/:id/index.ts
@@ -1,45 +1,87 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
-import { studentResponseSchema, studentSelectSchema } from "../../../schema";
+import {
+  studentResponseSchema,
+  studentSelectSchema,
+  studentWriteSchema,
+} from "../../../schema";
+import _ from "lodash";
+import { updateStudentField } from "..";
 const id = new OpenAPIHono();
-id.get('', (c) => {
-  
+id.get("", (c) => {
   return c.json({ message: `Details for student ${id}` });
 });
-id.openapi({
-  path: "",
-  method: "get",
-  responses: {
-    200: {
-      content: {
-        "application/json": {
-          schema: studentResponseSchema
-        }
+id.openapi(
+  {
+    path: "",
+    method: "get",
+    responses: {
+      200: {
+        content: {
+          "application/json": {
+            schema: studentResponseSchema,
+          },
+        },
+        description: "Retrieve one student",
       },
-      description: "Retrieve one student"
-    }
-  },
-  request: {}
-}, async (c) => {
-  const id = c.req.param('id');
-  const prisma = c.get('prisma');
-  const result = await prisma.student.findFirst({
-    where: {
-      id: {
-        equals: id,
-      }
     },
-    select: studentSelectSchema
-  });
-  return c.json({success: true, student: result!}, 200); //TODO: if null?
-});
+    request: {},
+  },
+  async (c) => {
+    const id = c.req.param("id");
+    const prisma = c.get("prisma");
+    const result = await prisma.student.findFirst({
+      where: {
+        id: {
+          equals: id,
+        },
+      },
+      select: studentSelectSchema,
+    });
+    return c.json({ success: true, student: result! }, 200); //TODO: if null?
+  }
+);
 // [수정] 특정 학생 정보 업데이트
-id.patch('', (c) => {
-  const id = c.req.param('id');
-  return c.json({ message: `Update student ${id}` });
-});
+id.openapi(
+  {
+    path: "",
+    method: "patch",
+    request: {
+      body: {
+        content: {
+          "application/json": {
+            schema: studentWriteSchema,
+          },
+        },
+      },
+    },
+    responses: {
+      200: {
+        content: {
+          "application/json": {
+            schema: studentResponseSchema,
+          },
+        },
+        description: "",
+      },
+    },
+  },
+  async (c) => {
+    const id = c.req.param("id");
+    const { student } = c.req.valid("json");
+    const prisma = c.get("prisma");
+    const result = await prisma.student.update({
+      where: {
+        id: id,
+      },
+      select: studentSelectSchema,
+      data: updateStudentField(student),
+    });
+    return c.json({ success: true, student: result! }, 200);
+  }
+);
 // [삭제] 특정 학생 정보 삭제
-id.delete('', (c) => {
-  const id = c.req.param('id');
+id.delete("", (c) => {
+  const id = c.req.param("id");
   return c.json({ message: `Delete student ${id}` });
 });
 export { id };
