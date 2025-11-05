1782a7bf33f81155b7f0022477015596b19fb3e0
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-11-05 17:02:00 +0900

    Return individual student

--- apps/worker/src/routes/students/:id/index.ts
+++ apps/worker/src/routes/students/:id/index.ts
@@ -1,8 +1,36 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
+import { studentResponseSchema, studentSelectSchema } from "../../../schema";
 const id = new OpenAPIHono();
 id.get('', (c) => {
-  const id = c.req.param('id');
+  
   return c.json({ message: `Details for student ${id}` });
+});
+id.openapi({
+  path: "",
+  method: "get",
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: studentResponseSchema
+        }
+      },
+      description: "Retrieve one student"
+    }
+  },
+  request: {}
+}, async (c) => {
+  const id = c.req.param('id');
+  const prisma = c.get('prisma');
+  const result = await prisma.student.findFirst({
+    where: {
+      id: {
+        equals: id,
+      }
+    },
+    select: studentSelectSchema
+  });
+  return c.json({success: true, student: result!}, 200); //TODO: if null?
 });
 // [수정] 특정 학생 정보 업데이트
 id.patch('', (c) => {
