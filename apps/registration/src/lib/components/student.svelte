6f0765d6e2cb9739441e74eb5382e207fc6c29f1
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-11-14 18:27:14 +0900

    Get students default input infos

---
+++ apps/registration/src/lib/components/student.svelte
@@ -0,0 +1,10 @@
+<div class="flex flex-row space-x-1.5">
+    <div class="input-group grid-cols=[auto_1fr_auto] flex flex-row w-5xl">
+        <input class="input" type="text" placeholder="First name..."/>
+        <input class="input" type="text" placeholder="Last name..." />
+    </div>
+    <span class="vr border-1-4"></span>
+    <label class="label flex flex-row">
+        <input class="input" type="number" step="0.1" placeholder="GPA..." min={0} max={4.5}/>
+    </label>
+</div>
