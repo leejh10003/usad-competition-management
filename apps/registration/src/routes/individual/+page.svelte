b7981888fdfef9d84be4a742b94053239d1d0f94
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-11-16 02:19:31 +0900

    Append studentrow component

--- apps/registration/src/routes/individual/+page.svelte
+++ apps/registration/src/routes/individual/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import StudentRow from "$lib/components/student.svelte";
 </script>
-<form class="mx-auto w-full max-w-md space-x-4">
+<form class="mx-auto w-full max-w-md space-x-4 p-11">
     <StudentRow />
 </form>
