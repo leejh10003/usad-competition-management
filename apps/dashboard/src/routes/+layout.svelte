3f233adbe12b75fee403404b48ea8be64a103450
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-11-16 02:52:07 +0900

    Add menu bar and items

--- apps/dashboard/src/routes/+layout.svelte
+++ apps/dashboard/src/routes/+layout.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	import { CalendarIcon, CircleUserIcon, MenuIcon, SearchIcon } from '@lucide/svelte';
-	import { AppBar } from '@skeletonlabs/skeleton-svelte';
+	import { CalendarIcon, CircleUserIcon, SearchIcon, SchoolIcon, UserCheckIcon, IdCardLanyardIcon, UsersIcon, UserIcon } from '@lucide/svelte';
+	import { AppBar, Navigation } from '@skeletonlabs/skeleton-svelte';
 
 	let { children } = $props();
+	let anchorSidebar = 'btn hover:preset-tonal justify-start px-2 w-full';
 </script>
 
-<AppBar>
+<div class="flex flex-col h-full">
+	<AppBar>
 	<AppBar.Toolbar class="grid-cols-[auto_1fr_auto]">
 		<AppBar.Lead>
-			<button type="button" class="btn-icon btn-icon-lg hover:preset-tonal"><MenuIcon /></button>
 		</AppBar.Lead>
 		<AppBar.Headline>
-			<p class="text-2xl">Skeleton</p>
+			<p class="text-2xl">USAD Final Management System</p>
 		</AppBar.Headline>
 		<AppBar.Trail>
 			<button type="button" class="btn-icon hover:preset-tonal"><SearchIcon class="size-6" /></button>
@@ -22,5 +23,41 @@
 		</AppBar.Trail>
 	</AppBar.Toolbar>
 </AppBar>
-
+<div class="flex flex-row h-full">
+	<Navigation layout="sidebar" class="grid grid-rows-[auto_1fr_auto] gap-4">
+	<Navigation.Content>
+		<Navigation.Group>
+			<Navigation.Label>Participants</Navigation.Label>
+			<Navigation.Menu class={anchorSidebar}>
+				<SchoolIcon />
+				<span>School</span>
+			</Navigation.Menu>
+			<Navigation.Menu class={anchorSidebar}>
+				<IdCardLanyardIcon />
+				<span>Coach</span>
+			</Navigation.Menu>
+			<Navigation.Menu class={anchorSidebar}>
+				<UsersIcon />
+				<span>Team</span>
+			</Navigation.Menu>
+			<Navigation.Menu class={anchorSidebar}>
+				<UserIcon />
+				<span>Student</span>
+			</Navigation.Menu>
+		</Navigation.Group>
+		<Navigation.Group>
+			<Navigation.Label>Events</Navigation.Label>
+			<Navigation.Menu class={anchorSidebar}>
+				<CalendarIcon />
+				<span>Event</span>
+			</Navigation.Menu>
+			<Navigation.Menu class={anchorSidebar}>
+				<UserCheckIcon />
+				<span>Check in</span>
+			</Navigation.Menu>
+		</Navigation.Group>
+	</Navigation.Content>
+</Navigation>
 {@render children()}
+</div>
+</div>
