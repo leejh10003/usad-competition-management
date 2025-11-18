a84a341f70ac66f9ed45708e257aeb09beb14588
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-11-19 03:15:26 +0900

    Adjust field sizes

--- apps/registration/src/lib/components/student.svelte
+++ apps/registration/src/lib/components/student.svelte
@@ -5,12 +5,7 @@
     const { student = $bindable<Student>() } = $props<{student: Student}>();
 </script>
 <div class="flex flex-row space-x-1.5">
-    <div class="input-group grid-cols=[auto_1fr_auto] flex flex-row w-5xl">
-        <input class="input" type="text" placeholder="First name..." bind:value={student.firstName}/>
-        <input class="input" type="text" placeholder="Last name..." bind:value={student.lastName}/>
-    </div>
-    <span class="vr border-1-4"></span>
-    <label class="label flex flex-row">
-        <input class="input" type="number" step="0.1" placeholder="GPA..." min={0} max={4.5} bind:value={student.gpa}/>
-    </label>
+    <input class="input flex-5" type="text" placeholder="First name..." bind:value={student.firstName}/>
+    <input class="input flex-5" type="text" placeholder="Last name..." bind:value={student.lastName}/>
+    <input class="input flex-2" type="number" step="0.1" placeholder="GPA..." min={0} max={4.5} bind:value={student.gpa}/>
 </div>
