3f1e5b34e5a74252b14585a6328f90080b40df7d
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-11-19 03:01:03 +0900

    Route user to school view

--- apps/registration/src/routes/+page.svelte
+++ apps/registration/src/routes/+page.svelte
@@ -6,7 +6,7 @@
         <h1 class="h1">Are you coach from school or individualy participating?</h1>
         <br />
         <nav class="btn-group flex-col p-2 md:flex-row w-min">
-            <button type="button" class="btn preset-filled-primary-500 btn-lg">I'm coach from school</button>
+            <button type="button" class="btn preset-filled-primary-500 btn-lg" onclick={() => goto('/school')}>I'm coach from school</button>
             <button type="button" class="btn preset-outlined-primary-500 btn-lg" onclick={() => goto('/individual')}>I'm participating individualy</button>
         </nav>
     </div>
