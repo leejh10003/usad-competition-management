f17a012bdef1068761ecd99fe0171836afd7d53f
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-11-19 00:36:18 +0900

    Add optional address fields to student

---
+++ packages/database/migrations/cockroachdb/20251118152434_add_optional_address_to_student/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "students" ADD COLUMN     "city" STRING;
+ALTER TABLE "students" ADD COLUMN     "state" STRING;
+ALTER TABLE "students" ADD COLUMN     "street_address" STRING;
+ALTER TABLE "students" ADD COLUMN     "zip_code" STRING;
