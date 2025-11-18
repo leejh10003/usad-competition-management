e9a64c4ba9e5a40e3bd1eb18a7fe28ae16a7e5b5
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-11-19 02:56:10 +0900

    Make student bindable

--- apps/registration/src/lib/components/student.svelte
+++ apps/registration/src/lib/components/student.svelte
@@ -1,10 +1,16 @@
+<script lang="ts">
+    import { teamInsertSchema } from 'usad-scheme';
+	import type z from 'zod';
+    type Student = z.infer<typeof teamInsertSchema>['team']['students'][number];
+    const { student = $bindable() } = $props<{student: Student}>();
+</script>
 <div class="flex flex-row space-x-1.5">
     <div class="input-group grid-cols=[auto_1fr_auto] flex flex-row w-5xl">
-        <input class="input" type="text" placeholder="First name..."/>
-        <input class="input" type="text" placeholder="Last name..." />
+        <input class="input" type="text" placeholder="First name..." bind:value={student.firstName}/>
+        <input class="input" type="text" placeholder="Last name..." bind:value={student.lastName}/>
     </div>
     <span class="vr border-1-4"></span>
     <label class="label flex flex-row">
-        <input class="input" type="number" step="0.1" placeholder="GPA..." min={0} max={4.5}/>
+        <input class="input" type="number" step="0.1" placeholder="GPA..." min={0} max={4.5} bind:value={student.gpa}/>
     </label>
 </div>
