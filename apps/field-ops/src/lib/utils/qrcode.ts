77dccacb54eb33e7d1ba0363d7b1cb169095fc65
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-11-24 04:59:33 +0900

    Add qr code decoding logic

---
+++ apps/field-ops/src/lib/utils/qrcode.ts
@@ -0,0 +1,15 @@
+export function getValidIdStringFromQrCode(e: string, path: string, paramName: string){
+    try {
+        const url = new URL(e);
+        if (url.pathname.replaceAll(/^\//g, '').replaceAll(/\/$/g, '') !== path.replaceAll(/^\//g, '').replaceAll(/\/$/g, '')) {
+            return null;
+        };
+        const search = url.searchParams;
+        if (!search.get(paramName)){
+            return null;
+        }
+        return search.get(paramName);
+    } catch {
+        return e;
+    }
+}
--- apps/field-ops/src/routes/check-in-team/+page.svelte
+++ apps/field-ops/src/routes/check-in-team/+page.svelte
@@ -3,13 +3,18 @@
 	import QrCodeScanner from '$lib/components/qrcode-scanner.svelte';
 	import { goto } from '$app/navigation';
 	import BottomPadding from '$lib/components/bottom-padding.svelte';
+	import _ from 'lodash';
+	import { getValidIdStringFromQrCode } from '$lib/utils/qrcode';
 </script>
 
 <div class="flex grow flex-col">
 	<QrCodeScanner
 		detect={(e, scanner) => {
-			scanner.pause();
-			goto(`/check-in-team/scanned?id=${e.decodedText}`)
+			const id = getValidIdStringFromQrCode(e.decodedText, 'check-in-team/scanned', 'id');
+			if (id) {
+				scanner.pause();
+				goto(`/check-in-team/scanned?id=${id}`)
+			}
 		}}
 		error={(e) => console.log({ type: 'ERROR', message: e })}
 		paused={false}
