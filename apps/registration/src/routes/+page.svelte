4e6a2211aeaefc3968e191e3e0be2f761f1cd73b
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-11-24 03:59:55 +0900

    Debug button press

--- apps/registration/src/routes/+page.svelte
+++ apps/registration/src/routes/+page.svelte
@@ -5,10 +5,16 @@
     <div class="m-auto flex flex-col items-center text-center">
         <h1 class="h1">Are you registering a Team?</h1>
         <br />
-        <button type="button" class="btn preset-filled-primary-500 btn-lg" onclick={() => goto('/school')}>Team</button>
+        <button type="button" class="btn preset-filled-primary-500 btn-lg" onclick={() => {
+            console.log("pressing");
+            goto('/school');
+        }}>Team</button>
         <br />
         <h1 class="h1">Are you registering as an Individual?</h1>
         <br />
-        <button type="button" class="btn preset-filled-primary-500 btn-lg" onclick={() => goto('/individual')}>Individual</button>
+        <button type="button" class="btn preset-filled-primary-500 btn-lg" onclick={() => {
+            console.log("pressing");
+            goto('/individual');
+        }}>Individual</button>
     </div>
 </div>
