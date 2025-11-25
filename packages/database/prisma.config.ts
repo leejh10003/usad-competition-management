e74fe8a766f5e8685077e8d452bdb19bf1597e9c
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-11-26 05:17:33 +0900

    Lint database

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -1,11 +1,11 @@
-import { PrismaConfig } from 'prisma';
+import { PrismaConfig } from "prisma";
 
 export default {
-    experimental: {
-        adapter: true,
-    },
-    schema: './src/prisma/schema.prisma',
-    migrations: {
-        path: './migrations/cockroachdb'
-    },
+  experimental: {
+    adapter: true,
+  },
+  schema: "./src/prisma/schema.prisma",
+  migrations: {
+    path: "./migrations/cockroachdb",
+  },
 } satisfies PrismaConfig;
