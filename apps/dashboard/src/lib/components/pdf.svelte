2b87a27a4003b796f5dbd1294f22d6995b288f9c
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-11-26 02:00:42 +0900

    Add loading for pdf

--- apps/dashboard/src/lib/components/pdf.svelte
+++ apps/dashboard/src/lib/components/pdf.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import * as pdfjs from 'pdfjs-dist';
+  import {disableNonNumeric} from '$lib/utils/validation'
   import { Pagination } from '@skeletonlabs/skeleton-svelte';
 	import { ArrowLeftIcon, ArrowRightIcon } from '@lucide/svelte';
   // Import the worker if you're not using a CDN for it
   // import * as pdfWorker from 'pdfjs-dist/build/pdf.worker.mjs'; 
 
   const { data } = $props<{data: Parameters<typeof pdfjs['getDocument']>[0]}>();
+  type LoadState = 'error' | 'loaded' | 'loading';
 
   let canvasRef: HTMLCanvasElement;
   let pdfDoc: pdfjs.PDFDocumentProxy | null = null;
   let currentPageNum = $state(1);
   let maxPages = $state(0);
+  let loading = $state<LoadState>('loading');
 
   onMount(async () => {
     // Set the worker source for pdf.js
@@ -21,9 +24,11 @@
     try {
       const loadingTask = pdfjs.getDocument(data);
       pdfDoc = await loadingTask.promise;
+      loading = 'loaded';
       maxPages = pdfDoc.numPages;
       renderPage(currentPageNum);
     } catch (error) {
+      loading = 'error';
       console.error('Error loading PDF:', error);
     }
   });
@@ -78,31 +83,45 @@
 </script>
 
 <div>
-  <canvas bind:this={canvasRef}></canvas>
-  <div class="flex flex-row">
-    <Pagination count={maxPages} pageSize={1} page={currentPageNum}>
-        <Pagination.PrevTrigger onclick={() => prevPage()}><ArrowLeftIcon class="size-4"/></Pagination.PrevTrigger>
-        <Pagination.Context>
-            {#snippet children(pagination)}
-                {#each pagination().pages as page, index (page)}
-					{#if page.type === 'page'}
-						<Pagination.Item onclick={(e) => selectPage(page.value)} {...page}>
-							{page.value}
-						</Pagination.Item>
-					{:else}
-						<Pagination.Ellipsis {index}>&#8230;</Pagination.Ellipsis>
-					{/if}
-				{/each}
-            {/snippet}
-        </Pagination.Context>
-        <Pagination.NextTrigger onclick={() => nextPage()}><ArrowRightIcon class="size-4"/></Pagination.NextTrigger>
-    </Pagination>
-    <input type="number" class="input" min="1" step="1" max={maxPages} bind:value={currentPageNum} onchange={(e) => {
-      if (e.type === "change") {
-        var page = parseInt((e.target as HTMLInputElement).value) as number;
-        currentPageNum = page;
-        renderPage(currentPageNum);
-      }
-    }} />
+  {#if loading === 'loading'}
+  <div class="space-y-4">
+		<div class="placeholder animate-pulse"></div>
+		<div class="placeholder animate-pulse"></div>
+		<div class="placeholder animate-pulse"></div>
+		<div class="placeholder animate-pulse"></div>
+	</div>
+  {:else if loading === 'error'}
+  <div>
+    PDF Load failed
+  </div>
+  {/if}
+  <div class={loading === 'loaded' ? 'visible' : 'hidden'}>
+    <canvas bind:this={canvasRef}></canvas>
+    <div class="flex flex-row">
+      <Pagination count={maxPages} pageSize={1} page={currentPageNum}>
+          <Pagination.PrevTrigger onclick={() => prevPage()}><ArrowLeftIcon class="size-4"/></Pagination.PrevTrigger>
+          <Pagination.Context>
+              {#snippet children(pagination)}
+                  {#each pagination().pages as page, index (page)}
+            {#if page.type === 'page'}
+              <Pagination.Item onclick={(e) => selectPage(page.value)} {...page}>
+                {page.value}
+              </Pagination.Item>
+            {:else}
+              <Pagination.Ellipsis {index}>&#8230;</Pagination.Ellipsis>
+            {/if}
+          {/each}
+              {/snippet}
+          </Pagination.Context>
+          <Pagination.NextTrigger onclick={() => nextPage()}><ArrowRightIcon class="size-4"/></Pagination.NextTrigger>
+      </Pagination>
+      <input type="number" class="input" onkeypress={disableNonNumeric} min="1" step="1" max={maxPages} bind:value={currentPageNum} onchange={(e) => {
+        if (e.type === "change") {
+          var page = parseInt((e.target as HTMLInputElement).value) as number;
+          currentPageNum = page;
+          renderPage(currentPageNum);
+        }
+      }} />
+    </div>
   </div>
 </div>
---
+++ apps/dashboard/src/lib/utils/validation.ts
@@ -0,0 +1,5 @@
+export function disableNonNumeric(e: KeyboardEvent){
+    if (e.code.startsWith('Key')){
+        e.preventDefault();
+    }
+}
