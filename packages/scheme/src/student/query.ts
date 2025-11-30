c78adb7ab08d511577eba0df2eed9019952033c5
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-12-01 03:23:13 +0900

    Add type for query

--- packages/scheme/src/student/query.ts
+++ packages/scheme/src/student/query.ts
@@ -2,8 +2,8 @@ import { z } from "@hono/zod-openapi";
 import { Prisma } from "@prisma/client";
 
 export const studentQuerySchema = z.object({
-  orderBy: z.coerce.string().transform((str) => str ? JSON.parse(str) as Prisma.StudentFindManyArgs['orderBy'] : undefined),
-  take: z.coerce.number().int().min(1).max(100).optional().default(10),
-  skip: z.coerce.number().int().min(0).optional().default(0),
-  where: z.coerce.string().transform((str) => str ? JSON.parse(str) as Prisma.StudentFindManyArgs['where'] : undefined)
+  orderBy: z.coerce.string().optional().transform((str) => str ? JSON.parse(str) as Prisma.StudentFindManyArgs['orderBy'] : undefined).optional(),
+  take: z.coerce.number().int().min(1).max(100).optional().default(10).optional(),
+  skip: z.coerce.number().int().min(0).optional().default(0).optional(),
+  where: z.coerce.string().optional().transform((str) => str ? JSON.parse(str) as Prisma.StudentFindManyArgs['where'] : undefined).optional(),
 });
