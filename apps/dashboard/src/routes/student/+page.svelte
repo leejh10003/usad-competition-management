93c6de7954de08bfcc12695a95614c4c09d11b16
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-12-02 00:28:23 +0900

    Add filter and query functionality

--- apps/dashboard/src/routes/student/+page.svelte
+++ apps/dashboard/src/routes/student/+page.svelte
@@ -2,30 +2,63 @@
 	//eslint-disable-next-line @typescript-eslint/no-unused-vars
 	import { goto } from '$app/navigation';
 	import { page } from '$app/state';
-	import { Dialog, Portal, Pagination } from '@skeletonlabs/skeleton-svelte';
-	import _ from 'lodash';
-	import { division, studentQuerySchema, studentResponseSchema } from 'usad-scheme';
-	import { ArrowLeftIcon, ArrowRightIcon, XIcon } from '@lucide/svelte';
+	import { Dialog, Portal, Pagination, Collapsible } from '@skeletonlabs/skeleton-svelte';
+	import _, { debounce } from 'lodash';
+	import { studentQuerySchema, studentResponseSchema } from 'usad-scheme';
+	import { ArrowLeftIcon, ArrowRightIcon, ArrowUpDownIcon, XIcon } from '@lucide/svelte';
 	import z from 'zod';
 	import Pdf from '$lib/components/pdf.svelte';
 	import { workerRequest } from '$lib/api/test';
+	import { resolve } from '$app/paths';
 	type StudentResponseItem = z.infer<typeof studentResponseSchema>['student'];
 	var isLoading = $state<boolean>(true);
 	var isFirstLoaded = $state<boolean>(true);
-	var limit = $state<number>(10);
-	var currentPage = $state<number>(1);
-	var offset = $derived.by(() => (currentPage - 1) * limit);
-	var total = $state<number>(0);
-	var currentCount = $state<number>(0);
 	var students = $state<StudentResponseItem[]>([]);
+	var currentCount = $state<number>(0);
+	const query = $derived.by(() => page.url.searchParams);
+	const getLimit = $derived.by(() => {
+		const limit = query.get('limit');
+		return limit ? parseInt(limit) : 10;
+	})
+	function setLimit(input: number) {
+		const route = page.url.pathname;
+		const params = _currentParam();
+		params.set('limit', input.toString());
+		const going = `/${route.replace(/^\//g, '')}${params.size > 0 ? `?${params.toString()}` : ''}` as Parameters<typeof resolve>[0];
+		goto(resolve(going))
+	}
+	function _currentParam() {
+		const limit = query.get('limit');
+		const currentPage = query.get('currentPage');
+		const params = new URLSearchParams();
+		try {
+			params.set('limit', parseInt(decodeURI(limit!)).toString());
+		} catch (e) {}
+		if (currentPage && decodeURI(currentPage as string).trim().length > 0) {
+			params.set('currentPage', decodeURI(currentPage as string));
+		}
+		return params;
+	}
+	const getCurrentPage = $derived.by(() => {
+		const currentPage = query.get('currentPage');
+		return currentPage ? parseInt(currentPage) : 1;
+	})
+	function setCurrentPage(input: number) {
+		const route = page.url.pathname;
+		const params = _currentParam();
+		params.set('currentPage', input.toString());
+		const going = `/${route.replace(/^\//g, '')}${params.size > 0 ? `?${params.toString()}` : ''}` as Parameters<typeof resolve>[0];
+		goto(resolve(going))
+	}
+	const offset = $derived.by(() => (getCurrentPage - 1) * getLimit);
+	var total = $state<number>(0);
 	const animation =
 		'transition transition-discrete opacity-0 translate-y-[100px] starting:data-[state=open]:opacity-0 starting:data-[state=open]:translate-y-[100px] data-[state=open]:opacity-100 data-[state=open]:translate-y-0';
-	//eslint-disable-next-line @typescript-eslint/no-unused-vars
-	async function fetch(searchParams: z.infer<typeof studentQuerySchema>) {
+	async function fetch() {
 		isLoading = true;
 		//TODO: server fetch
 		const {result, count} = await workerRequest.getStudent({
-			take: limit,
+			take: getLimit,
 			skip: offset
 		});
 		students = result;
@@ -34,24 +67,52 @@
 		isLoading = false;
 	}
 	$effect(() => {
+		const entries = Object.fromEntries(page.url.searchParams.entries());
+		for (const [key, value] of Object.entries(entries)) {
+			try {
+				entries[key] = decodeURIComponent(value as string);
+			} catch (e) {
+				entries[key] = value;
+			}
+		}
 		let searchParams = studentQuerySchema.safeParse(
-			Object.fromEntries(page.url.searchParams.entries())
+			entries
 		);
 		if (searchParams.success) {
-			fetch(searchParams.data);
+			fetch();
 		} else {
 			//TODO
 		}
 	});
-	/*function changeFilter() {
-		const searchParams = page.url.searchParams;
-		searchParams.set('test', 'true');
-		goto(`?${searchParams.toString()}`);
-	}*/
 </script>
 
 <div class="flex h-full w-full flex-col gap-y-3.5 p-8">
 	<h1 class="h1 font-bold">Student</h1>
+	<Collapsible class="rounded-xs border border-primary-100 p-4">
+		<div class="flex w-full items-center justify-between">
+			<p class="font-bold">Actions</p>
+			<Collapsible.Trigger class="btn-icon hover:preset-tonal">
+				<ArrowUpDownIcon class="size-4" />
+			</Collapsible.Trigger>
+		</div>
+		<Collapsible.Content class="grid w-full grid-cols-3 gap-1">
+			<label class="label">
+				<span class="label-text">Students Per Page</span>
+				<input
+					class="input"
+					type="number"
+					onchange={(e) => {
+						if (_.isNumber(e.currentTarget.value)) {
+							setLimit(e.currentTarget.value);
+						} else {
+							setLimit(parseInt(e.currentTarget.value));
+						}
+					}}
+					value={getLimit}
+				/>
+			</label>
+		</Collapsible.Content>
+	</Collapsible>
 	<table class="table">
 		<thead>
 			<tr>
@@ -65,7 +126,7 @@
 		</thead>
 		<tbody>
 			{#if isLoading}
-				{#each _.range(0, limit, 1) as n (n)}
+				{#each _.range(0, getLimit, 1) as n (n)}
 					<tr>
 						<td><div class="placeholder w-full animate-pulse">&nbsp;</div></td>
 						<td><div class="placeholder w-full animate-pulse">&nbsp;</div></td>
@@ -146,13 +207,13 @@
 			</tr>
 		</tfoot>
 	</table>
-	<Pagination count={total} pageSize={limit} page={currentPage}>
-		<Pagination.PrevTrigger onclick={() => currentPage--}><ArrowLeftIcon class="size-4" /></Pagination.PrevTrigger>
+	<Pagination count={total} pageSize={getLimit} page={getCurrentPage}>
+		<Pagination.PrevTrigger onclick={() => setCurrentPage(getCurrentPage - 1)}><ArrowLeftIcon class="size-4" /></Pagination.PrevTrigger>
 		<Pagination.Context>
 			{#snippet children(pagination)}
 				{#each pagination().pages as page, index (page)}
 					{#if page.type === 'page'}
-						<Pagination.Item onclick={(v) => {currentPage = page.value}} {...page}>
+						<Pagination.Item onclick={(v) => {setCurrentPage(page.value)}} {...page}>
 							{page.value}
 						</Pagination.Item>
 					{:else}
@@ -161,6 +222,6 @@
 				{/each}
 			{/snippet}
 		</Pagination.Context>
-		<Pagination.NextTrigger onclick={() => {currentPage++}}><ArrowRightIcon class="size-4" /></Pagination.NextTrigger>
+		<Pagination.NextTrigger onclick={() => setCurrentPage(getCurrentPage + 1)}><ArrowRightIcon class="size-4" /></Pagination.NextTrigger>
 	</Pagination>
 </div>
