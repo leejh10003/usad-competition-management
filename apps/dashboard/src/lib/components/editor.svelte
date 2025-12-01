49c4eabf61015e99ef2725bc5e53891232bd2203
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-12-01 14:09:13 +0900

    Import css without runtime bundling

--- apps/dashboard/src/lib/components/editor.svelte
+++ apps/dashboard/src/lib/components/editor.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { onMount } from "svelte";
-    import type Quill from "quill";
+    import Quill from "quill";
     import type { QuillOptions } from "quill";
+    import 'quill/dist/quill.snow.css';
     const props = $props<{ }>();
     let editor: HTMLDivElement;
     let options: QuillOptions = {
@@ -19,18 +20,16 @@
     };
     let quill: Quill;
     export {quill} ;
-    onMount(async () => {
-        const {default: Quill} = await import("quill");
+    onMount(() => {
         quill = new Quill(editor, options);
     });
 </script>
 <style>
-    @import 'https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.snow.css';
     .ql-editor {
         min-height: 30vh;
         max-height: 30vh;
         height: 30vh;
-        overflow: auto;
+        overflow: scroll;
         width: 100%;
         max-width: 100%;
         min-width: 100%;
