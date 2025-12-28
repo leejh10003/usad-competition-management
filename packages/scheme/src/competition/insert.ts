ac3628d7d765f0e384854a3dc65e9ff09ffc922d
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-12-28 12:53:10 -0500

    Add nested insert to competition

--- packages/scheme/src/competition/insert.ts
+++ packages/scheme/src/competition/insert.ts
@@ -1,8 +1,13 @@
 import { z } from "@hono/zod-openapi";
 import { basicOptionalInfos, basicRequiredInfos } from "./baseTypes";
+import { eventsInsertSchema } from "../event";
+import { schoolsInsertSchema } from "../school";
 
 
-export const competitionInsertItem = basicRequiredInfos.extend(basicOptionalInfos.shape);
+export const competitionInsertItem = basicRequiredInfos
+  .extend(basicOptionalInfos.shape)
+  .extend(eventsInsertSchema.shape)
+  .extend(schoolsInsertSchema.shape);
 export const competitionInsertSchema = z.object({
   competition: competitionInsertItem,
 });
