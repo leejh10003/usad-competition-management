8fdc08da65343b99d025676d2eacca4df536a923
Lee, JunHyuk <leejh10003@poc-holdings.com>
2025-12-29 06:25:15 -0500

    Componentize dialog appear animation

---
+++ apps/dashboard/src/lib/utils/animation.ts
@@ -0,0 +1,3 @@
+const dialogAppearAnimation =
+	'transition transition-discrete opacity-0 translate-y-[100px] starting:data-[state=open]:opacity-0 starting:data-[state=open]:translate-y-[100px] data-[state=open]:opacity-100 data-[state=open]:translate-y-0';
+export { dialogAppearAnimation };
