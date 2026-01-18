75fb8ca2d3ff2da127fb4fe6cb9849738853adf6
Lee, JunHyuk <leejh10003@poc-holdings.com>
2026-01-18 13:16:21 -0500

    Componentize search text highlight

---
+++ apps/dashboard/src/lib/components/searched-text.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { splitStringForQueryHighlight } from "$lib/utils/string";
+
+    const { splitted } = $props<{splitted: ReturnType<typeof splitStringForQueryHighlight>}>();
+</script>
+<div class="flex flex-row">
+{#each splitted as { originalQuery, originalString }, i (i)}
+    <span class="whitespace-pre">{originalString}</span>
+    <span class="bg-blue-500 whitespace-pre">{originalQuery}</span>
+{/each}
+</div>
